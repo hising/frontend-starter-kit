dad55489df71ddf896977acb0f4147ec6ceee8b3
Mattias Hising <hising@gmail.com>
2019-01-30 16:13:15 +0100

    WiP - Get things up

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
-module.exports = {
-    entry: [],
-    output: {},
-    devtool: "",
-    devServer: {
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpackDevServerPort = 3000;
 
+module.exports = {
+    mode: process.env.NODE_ENV || 'development',
+    entry: [
+        `webpack-dev-server/client?http://localhost:${webpackDevServerPort}`,
+        'webpack/hot/only-dev-server',
+        './src/index.ts'
+    ],
+    output: {
+        path: path.resolve('dist'),
+        filename: '[name].js'
+    },
+    devtool: '',
+    devServer: {
+        port: webpackDevServerPort,
+        hot: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
     },
     resolve: {
-
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
     },
     module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ]
-    }
+        rules: [{test: /\.tsx?$/, loader: 'ts-loader'}]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './resources/index.html'
+        })
+    ]
 };
