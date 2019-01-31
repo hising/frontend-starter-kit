85d63ffb7121ae7997d50501c133d4796b8a2796
Mattias Hising <hising@gmail.com>
2019-01-31 09:24:18 +0100

    HttpCLient with Axios as transport

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,8 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
-class HttpClient {
+export class HttpClient {
     private transport: any;
-    constructor() {
-        this.transport = axios;
+    private readonly baseURL: string;
+
+    // TODO: Create a HttpTransport Interface
+    constructor(baseURL: string = "", transport: any = axios) {
+        this.transport = transport;
+        this.baseURL = baseURL;
+    }
+
+    private getUrl(path: string) {
+        return this.baseURL + path;
+    }
+
+    public async get(path: string, params: any = {}) {
+        return await this.transport.get(this.getUrl(path));
+    }
+
+    public async post(path: string, params: any = {}, headers: any = {}) {
+        return await this.transport.post(this.getUrl(path), params);
     }
 }
